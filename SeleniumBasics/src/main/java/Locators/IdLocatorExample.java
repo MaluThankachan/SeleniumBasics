8cd84ca9901d3b3df7e1f7622d7a790533e7bbd7
MaluL <MaluL@LAPTOP-G5IT7DRE>
2024-10-14 14:01:59 -0700

    second commit

---
+++ SeleniumBasics/src/main/java/Locators/IdLocatorExample.java
@@ -0,0 +1,21 @@
+package Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IdLocatorExample {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://facebook.com");
+		driver.manage().window().maximize();
+		WebElement emailidfield = driver.findElement(By.id("email")); // to identify web element
+		emailidfield.click();
+		emailidfield.sendKeys("testdata");
+
+	}
+
+}
